7d6aff54b43c7cddd1daa8b69cb2bf274406d1a5
Jeff Feenstra <feenstra32@gmail.com>
2026-03-12 13:42:12 +0100

    Step 3: Added the default product list and the store start dialogue

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from products import Product
 from store import Store
 
+# setup initial stock of inventory
 product_list = [Product("MacBook Air M2", price=1450, quantity=100),
                 Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                Product("Google Pixel 7", price=500, quantity=250),
-               ]
-
+                Product("Google Pixel 7", price=500, quantity=250)
+                ]
 best_buy = Store(product_list)
-products = best_buy.get_all_products()
-print(best_buy.get_total_quantity())
-print(best_buy.order([(products[0], 1), (products[1], 2)]))
+
+def start(store: Store):
+    show_main_menu()
+
+def show_main_menu():
+    # I didn't want to do this in a single print call
+    print("1. List all products in store")
+    print("2. Show total amount in store")
+    print("3. Make an order")
+    print("4. Quit")
+
+start(best_buy)
